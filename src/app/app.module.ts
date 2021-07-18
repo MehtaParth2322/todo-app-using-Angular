ca1368a49738a001cd5e70313e4770d01209fa1c
MehtaParth2322 <mehtaparth2322@gmail.com>
2021-07-19 00:09:19 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DisplayContainerComponent } from './MyComponents/display-container/display-container.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
